e2a6d389f844ba628cac189c4be5caa19817901e
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-10-10 11:13:09 +0800

    feat:添加统一resultCode

---
+++ src/main/java/com/fansos/miconvert/MiconvertApplication.java
@@ -0,0 +1,13 @@
+package com.fansos.miconvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiconvertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiconvertApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/fansos/miconvert/constans/ResultCodeEnum.java
@@ -0,0 +1,35 @@
+package com.fansos.miconvert.constans;
+
+public enum ResultCodeEnum {
+    /**
+     * 响应结果码
+     */
+    CUSTOM_SIMPLE_ERROR_MESSAGE(1, "自定义错误消息(不需要用户确定,过几秒后消失)"),
+    SUCCESS(200, "响应成功"),
+    ERROR(500, "系统错误");
+
+
+    private Integer code;
+    private String message;
+
+    ResultCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
