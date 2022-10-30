6c9a822d969cb2577dbe000b55b4b19497d79d8b
0 <0@0>
2022-10-30 15:24:12 +0800

    fix:恢复图形验证码

--- src/main/java/com/fansos/miconvert/controller/SystemController.java
+++ src/main/java/com/fansos/miconvert/controller/SystemController.java
@@ -1,14 +1,21 @@
 package com.fansos.miconvert.controller;
 
+import com.fansos.miconvert.constant.ResultCodeEnum;
 import com.fansos.miconvert.model.pojo.UserInfo;
 import com.fansos.miconvert.model.result.Result;
 import com.fansos.miconvert.service.RedisService;
 import com.fansos.miconvert.service.SystemService;
+import com.fansos.miconvert.utils.CreateVerifiCodeImage;
 import com.fansos.miconvert.utils.JwtHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -63,24 +70,24 @@ public class SystemController {
 	@PostMapping("/login")
 	public Result login(@RequestBody UserInfo loginInfo, HttpServletRequest request) {
 
-		// //验证码校验
-		// //返回与该请求关联的当前 session 会话，或者如果请求没有 session 会话，则创建一个。
-		// HttpSession session = request.getSession();
-		// String sessionVerifiCode = (String) session.getAttribute("verifiCode");
-		// String loginVerifiCode = loginInfo.getVerifiCode();
-		//
-		// // session 中没有验证码，说明验证码过期
-		// if ("".equals(sessionVerifiCode) || null == sessionVerifiCode) {
-		// 	return Result.fail().message("验证码失效，刷新后重试");
-		// }
-		//
-		// // 用户输入的验证码和session中的验证码不一致，验证码输入错误
-		// if (!sessionVerifiCode.equalsIgnoreCase(loginVerifiCode)) {
-		// 	return Result.fail().message("验证码有误,请重新输入");
-		// }
-		//
-		// //从session域中移除现有的验证码
-		// session.removeAttribute("verifiCode");
+		//验证码校验
+		//返回与该请求关联的当前 session 会话，或者如果请求没有 session 会话，则创建一个。
+		HttpSession session = request.getSession();
+		String sessionVerifiCode = (String) session.getAttribute("verifiCode");
+		String loginVerifiCode = loginInfo.getVerifiCode();
+
+		// session 中没有验证码，说明验证码过期
+		if ("".equals(sessionVerifiCode) || null == sessionVerifiCode) {
+			return Result.fail().message("验证码失效，刷新后重试");
+		}
+
+		// 用户输入的验证码和session中的验证码不一致，验证码输入错误
+		if (!sessionVerifiCode.equalsIgnoreCase(loginVerifiCode)) {
+			return Result.fail().message("验证码有误,请重新输入");
+		}
+
+		//从session域中移除现有的验证码
+		session.removeAttribute("verifiCode");
 
 		//分用户类型进行校验
 		Map<Object, Object> map = new LinkedHashMap<>();
@@ -155,56 +162,56 @@ public class SystemController {
 	// 	}
 	// 	return Result.ok();
 	// }
-	//
-	//
-	// /**
-	//  * 解析前端带来的token，获取用户id,name
-	//  * @param token
-	//  * @return
-	//  */
-	// @GetMapping("/getInfo")
-	// public Result getInfoByToken(@RequestHeader("token") String token) {
-	// 	// 判断token是否有效
-	// 	boolean expiration = JwtHelper.isExpiration(token);
-	//
-	// 	if (expiration) {
-	// 		return Result.build(null, ResultCodeEnum.TOKEN_ERROR);
-	// 	}
-	// 	//从token解释出用户id。类型
-	// 	String userName = JwtHelper.getUserName(token);
-	//
-	// 	Map<String, Object> map = new LinkedHashMap<>();
-	//
-	// 	map.put("userName", userName);
-	// 	return Result.ok(map);
-	// }
-	//
-	// /**
-	//  * 1、获取验证码图片，字符串
-	//  * 2、装入session中
-	//  * 3、响应给浏览器
-	//  * @param request
-	//  * @param response
-	//  */
-	// @GetMapping("/getVerifiCodeImage")
-	// public void getVerifiCodeImage(HttpServletRequest request, HttpServletResponse response) {
-	//
-	// 	//获取图片
-	// 	BufferedImage verifiCodeImage = CreateVerifiCodeImage.getVerifiCodeImage();
-	//
-	// 	//获取图片上的验证码
-	// 	String verifiCode = String.valueOf(CreateVerifiCodeImage.getVerifiCode());
-	//
-	// 	//将验证码文本放入session域中，为下次验证做准备
-	// 	HttpSession session = request.getSession();
-	// 	session.setAttribute("verifiCode", verifiCode);
-	//
-	// 	//将验证码图片响应给浏览器
-	// 	try {
-	// 		ImageIO.write(verifiCodeImage, "JPEG", response.getOutputStream());
-	// 	} catch (IOException e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
+
+
+	/**
+	 * 解析前端带来的token，获取用户id,name
+	 * @param token
+	 * @return
+	 */
+	@GetMapping("/getInfo")
+	public Result getInfoByToken(@RequestHeader("token") String token) {
+		// 判断token是否有效
+		boolean expiration = JwtHelper.isExpiration(token);
+
+		if (expiration) {
+			return Result.build(null, ResultCodeEnum.TOKEN_ERROR);
+		}
+		//从token解释出用户id类型
+		Long userId = JwtHelper.getUserId(token);
+
+		Map<String, Object> map = new LinkedHashMap<>();
+
+		map.put("userId", userId);
+		return Result.ok(map);
+	}
+
+	/**
+	 * 1、获取验证码图片，字符串
+	 * 2、装入session中
+	 * 3、响应给浏览器
+	 * @param request
+	 * @param response
+	 */
+	@GetMapping("/getVerifiCodeImage")
+	public void getVerifiCodeImage(HttpServletRequest request, HttpServletResponse response) {
+
+		//获取图片
+		BufferedImage verifiCodeImage = CreateVerifiCodeImage.getVerifiCodeImage();
+
+		//获取图片上的验证码
+		String verifiCode = String.valueOf(CreateVerifiCodeImage.getVerifiCode());
+
+		//将验证码文本放入session域中，为下次验证做准备
+		HttpSession session = request.getSession();
+		session.setAttribute("verifiCode", verifiCode);
+
+		//将验证码图片响应给浏览器
+		try {
+			ImageIO.write(verifiCodeImage, "JPEG", response.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
